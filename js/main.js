05b217ced1e8dae9fef35c027935548214d85099
Chad Cuddigan <cuddigan@gmail.com>
2014-08-09 23:45:53 -0700

    Adding more content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var mouse = new THREE.Vector2(0, 0); 
 var mouseLerp = new THREE.Vector2(0, 0);
+var scrollPos = new THREE.Vector2(0, 0);
+
+var name = "CHAD CUDDIGAN";
 
 function start() {
   var time = 0;
@@ -10,8 +13,8 @@ function start() {
   var clock = new THREE.Clock();
 
   var renderer = new THREE.WebGLRenderer();
-  renderer.id = "glCanvas";
   renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.domElement.setAttribute( "id", "glCanvas" );
   document.body.appendChild( renderer.domElement );
 
   // make comet material 
@@ -32,12 +35,13 @@ function start() {
   // assemble the comet
   var comet = new THREE.Mesh( geometry, bgMaterial );
   comet.rotation.z = Math.random() * 360;
+  comet.position.y = 2;
   scene.add( comet );
 
   // add the dust planes
   for(var i = 0; i < 3; i++) {
     var dust = new THREE.Mesh( dustGeometry, dustMaterial );
-    dust.position.z = (i + 1) * 2.5;
+    dust.position.z = (i + 1) * 2.75;
     dust.position.x = Math.random() * 4;
     dust.position.y = Math.random() * 4;
     dust.rotation.z = Math.random() * 360;
@@ -52,14 +56,18 @@ function start() {
     // comet movement
     comet.rotation.z += 0.0006;
 
+    scrollPos.x = mouse.x;
+    scrollPos.y = mouse.y;
+    scrollPos.y -= $("body").scrollTop() * 2;
+
     // make the mouse lag a bit behind when it moves
-    mouseLerp.lerp(mouse, 0.05);
+    mouseLerp.lerp(scrollPos, 0.05);
 
     // camera position
     camera.position.x = Math.sin(time * 0.001) + mouseLerp.x * 0.001;
     camera.position.y = Math.sin(time * 0.0013) + mouseLerp.y * 0.001;
     camera.position.z = 10 + Math.sin(time * 0.05);
-    camera.rotation.z += 0.0001;
+    camera.rotation.z = time * 0.001;
 
     // move the dust planes
     for(i = 0; i < 3; i++) {
@@ -78,6 +86,17 @@ function start() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
   }, false);
+
+  function type(number) {
+    setTimeout(function() {
+      var n = name.substring(0, number);
+      n += "_";
+      $("#name").text(n).show();
+      if(number < name.length) type(number + 1);
+    }, 100);
+  }
+
+  //type(0);
 }
 
 document.addEventListener('mousemove', function(e) {
